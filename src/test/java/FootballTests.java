89b7e0151223ac24d6f8c0dc60d3b893ef0e8570
Dz B <baklanau@gmail.com>
2018-06-29 18:55:43 +0300

    23-24 lessons

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,7 +1,10 @@
 import config.TestConfig;
 import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -57,6 +60,51 @@ public class FootballTests extends TestConfig {
                         extract().response();
 
         String jsonResponseAsString = response.asString();
+
+    }
+
+    @Test
+    public void extractHeaders(){
+        Response response =
+                given().
+                        spec(footballReqSpec).
+                        when().
+                        get("competitions/426/teams").
+                        then().
+                        contentType(ContentType.JSON).
+                        extract().response();
+
+        Headers headers = response.getHeaders();
+
+        String contentType = response.getHeader("Content-Type");
+        System.out.println(contentType);
+    }
+
+    @Test
+    public void extractFirstTeamName(){
+
+        String firstTeamName = given().spec(footballReqSpec).when().get("competitions/426/teams").jsonPath().getString("teams.name[0]");
+
+        System.out.println(firstTeamName);
+    }
+
+    @Test
+    public void extractAllTeamNames(){
+
+        Response response =
+                given().
+                        spec(footballReqSpec).
+                        when().
+                        get("competitions/426/teams").
+                        then().
+                        contentType(ContentType.JSON).
+                        extract().response();
+
+        List<String> teamNames = response.path("teams.name");
+
+        for (String teamName:teamNames){
+            System.out.println(teamName);
+        }
 
     }
 
