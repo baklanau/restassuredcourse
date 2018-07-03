0904a51182bef70005b00d705fa3ad496b4d61b2
Dzmitry Baklanau <baklanau@gmail.com>
2018-07-04 02:57:41 +0300

    adding 30-31 lessons

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -2,12 +2,14 @@ import config.TestConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 
 public class GPathJSONTests extends TestConfig{
 
+    //29 lesson
     @Test
     public void extractMapOfElementsWithFind(){
 
@@ -18,4 +20,52 @@ public class GPathJSONTests extends TestConfig{
 
         System.out.println(allTeamDataForSingleTeam);
     }
+
+    //30 lesson
+    @Test
+    public void extractSingleValueWithFind(){
+
+        Response response = get("teams/66/players");
+
+        String certainPlayer = response.path
+                ("players.find{ it.jerseyNumber == 20 }.name");
+
+        System.out.println(certainPlayer);
+    }
+
+    //30 lesson
+    @Test
+    public void extractListOfValuesWithFindAll(){
+
+        Response response = get("teams/66/players");
+
+        List<String> playerNames = response.path("players.findAll { it.jerseyNumber > 10 }.name");
+
+        System.out.println(playerNames);
+    }
+
+    //31 lesson
+    @Test
+    public void extractSingleValueWithHighestNumber(){
+
+        Response response = get("teams/66/players");
+
+        String playerName = response.path("players.max { it.jerseyNumber }.name");
+
+        System.out.println(playerName);
+    }
+
+    //31 lesson
+    @Test
+    public void extractMultipleValuesAndSumThem(){
+
+        Response response = get("teams/66/players");
+
+        int sumOfJerseys = response.path("players.collect{ it.jerseyNumber }.sum()");
+
+        System.out.println(sumOfJerseys);
+
+    }
+
+
 }
